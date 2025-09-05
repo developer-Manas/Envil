4aece576ed649b5cccbe5e2bc56dc88a60bac840
developer-Manas <Manaskumar9102@gmail.com>
2025-09-05 09:31:50 +0530

    commmit-17

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -100,7 +100,7 @@ const ChatContainer = () => {
   ) : (
     <div className='flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden'>
         <img src={assets.logo_icon} className='max-w-16' alt="" />
-        <p className='text-lg font-medium text-white'>Chat anytime, anywhere</p>
+        <p className='text-lg font-medium text-white'>Privacy</p>
     </div>
   )
 }
